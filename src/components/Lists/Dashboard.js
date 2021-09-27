7438ca51918e1e39f9cbc03a101c4d29e7a91721
Zhan Lun Ng <zlun0817@gmail.com>
2021-09-27 18:07:29 +0800

    add simple single list demo

--- src/components/Lists/Dashboard.js
+++ src/components/Lists/Dashboard.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { getBoards } from '../../actions/boards'
 import { NewBoard } from '../Boards/NewBoard'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export const Dashboard = () => {
   const { boardId } = useParams()
@@ -39,6 +40,7 @@ export const Dashboard = () => {
               </button>
               <div>
                 {/* put lists and cards */}
+                <MyList />
               </div>
             </div>
           )
@@ -50,3 +52,91 @@ export const Dashboard = () => {
     </div>
   )
 }
+
+const initialItems = [
+  {
+    id: 'item-1',
+    name: 'item 1',
+  },
+  {
+    id: 'item-2',
+    name: 'item 2',
+  },
+  {
+    id: 'item-3',
+    name: 'item 3',
+  },
+  {
+    id: 'item-4',
+    name: 'item 4',
+  },
+]
+
+const MyList = () => {
+  const [items, setItems] = useState(initialItems)
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return
+    }
+
+    if (result.destination.index === result.source.index) {
+      return
+    }
+
+    const newItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    )
+
+    setItems(newItems)
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list-id-1">
+        {provided => (
+          <div
+            className="max-w-xs flex flex-col gap-2 p-4 rounded bg-gray-500 bg-opacity-30"
+            ref={provided.innerRef}
+            {...provided.droppableProps}>
+            <ItemList items={items} />
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+const ItemList = React.memo(({ items }) => {
+  return items.map((item, index) => (
+    <Item item={item} index={index} key={item.id} />
+  ))
+})
+
+const Item = ({ item, index }) => {
+  return (
+    <Draggable draggableId={item.id} index={index}>
+      {provided => (
+        <div
+          className="bg-gray-200 p-6 w-full mx-auto rounded shadow"
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+        >
+          {item.name}
+        </div>
+      )}
+    </Draggable>
+  )
+}
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
